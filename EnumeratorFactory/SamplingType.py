65e148f30587d7828c5a8789c73ffe75b115a7b7
Christin Di Maio <c.dimaio1@student.unisi.it>
2022-10-26 10:50:03 +0200

    Start refactoring

---
+++ EnumeratorFactory/SamplingType.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+def DownSamplingType(Enum):
+    TimePreserve = "timepreserve"
+    HalfSampling = "half"
+    
+def UpSamplingType(Enum):
+    TimePreserve = "timepreserve"
+    DoubleSampling = "double"
---
+++ download.py
@@ -0,0 +1,13 @@
+# import gdown 
+# pretrained_model = {
+#     "PretrainedStarGanv2":["https://drive.google.com/uc?id=1ML42wB0kHmYe9Mpw5jEh6Grwdpx9QynV"],
+#     "PreTrainedVocoder": ["https://drive.google.com/uc?id=1KoIOJiA4Zywm297uWdk1df9TDBxn-kX3"]
+# }
+
+# for index, value in pretrained_model.items():
+#     gdown.download(value[0],f"{index}.zip")
+
+
+import zipfile
+with zipfile.ZipFile("asd.zip", 'r') as zip_ref:
+    zip_ref.extractall("./Vocoder/hifi_gan_vctk")
