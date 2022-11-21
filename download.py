37b28774be9958b65fb34f16040ccb8598858be4
christianSistemiPos <dima9610@gmail.com>
2022-11-21 10:32:24 +0100

    added dataset download link

--- download.py
+++ download.py
@@ -1,12 +1,10 @@
-# import gdown 
-# pretrained_model = {
-#     "PretrainedStarGanv2":["https://drive.google.com/uc?id=1ML42wB0kHmYe9Mpw5jEh6Grwdpx9QynV"],
-#     "PreTrainedVocoder": ["https://drive.google.com/uc?id=1KoIOJiA4Zywm297uWdk1df9TDBxn-kX3"]
-# }
-
-# for index, value in pretrained_model.items():
-#     gdown.download(value[0],f"{index}.zip")
-
+import gdown 
+pretrained_model = {
+    "dataset":["https://drive.google.com/uc?id=1hQqfuvLe03G6Xx4B-t1uuRBbbNG7epCh"],
+    "PreTrainedVocoder": ["https://drive.google.com/uc?id=1KoIOJiA4Zywm297uWdk1df9TDBxn-kX3"]
+}
+for index, value in pretrained_model.items():
+    gdown.download(value[0],f"{index}.zip")
 
 import zipfile
 with zipfile.ZipFile("./datasets.zip", 'r') as zip_ref:
